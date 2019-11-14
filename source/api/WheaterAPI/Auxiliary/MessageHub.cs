c770a3342ea561f0d33ab52582037fcd306bcc2a
Gabriel Melo <GabrielC2@VERIFONE.com>
2019-11-13 21:04:32 -0300

    Added real time component: SignalR.

---
+++ source/api/WheaterAPI/Auxiliary/MessageHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WheaterAPI.Auxiliary
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessage(string username, string message) 
+        {
+            await Clients.All.SendAsync("ReceivedMessage", username, message);
+        }
+    }
+}
